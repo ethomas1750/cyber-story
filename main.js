bebc902cf1874ba571e51f446306da35929cb589
ethomas1750 <144511038+ethomas1750@users.noreply.github.com>
2024-06-08 23:50:55 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,30 @@
+const topstories = "https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty/v0/topstories"
 
+function fetchData(response){
+    let apiClips = `https://hacker-news.firebaseio.com/v0/item/${response}.json?print=pretty`
+    return fetch(apiClips)
+}
+
+fetch(topStories)
+    .then((response)=>{
+        return response.json()
+    })
+    .then((array)=>{
+        array.length = 100
+        let newArray = []
+        for(let item of array){
+            newArray.push(fetchData(item))
+        }
+        // console.log(newArray)
+        Promise.all(newArray)
+        .then((results) => Promise.all(results.map(res => res.json())))
+        .then((data)=>{
+            // console.log(data)
+            for (let item of data){
+                let clips = document.createElement('li')
+                clips.innerHTML = `<a href=${item.url}>${item.title}</a></br>By: ${item.by}, Score: ${item.score}, Comments: ${item.descendants}`
+                headlines.appendChild(clips)
+                console.log('hello')
+            }
+        })
+    })
